5ab118198937d62227b2e9a131f768cbb034618d
peb7268 <peb7268@gmail.com>
2017-04-02 13:34:41 -0400

    mounting ghost to express bug fixes

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 
 var express     = require('express');
 var path        = require('path');
+
 var app         = express();
-var ghost       = require(__dirname + '/public/insights/ghost-middleware');
+var parentApp   = express();    //This is needed for ghost
+var ghost       = require('ghost');
 
 //Middleware Configs
 //app.use(express.static(__dirname + '/public'));
-app.use( '/insights', ghost({
-	config: path.join(__dirname, 'insights/config.js')
-}) );
+ghost().then(function (ghostServer) {
+	console.log('ghost paths: ');
+	console.log(ghostServer.config.paths);
+    parentApp.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
+    ghostServer.start(parentApp);
+});
+
+// app.use( '/insights', ghost({
+// 	config: path.join(__dirname, 'insights/config.js')
+// }) );
+
 //If you want to use view engines
 //app.set('views', __dirname + '/views');
 //app.set('view engine', 'ejs');
@@ -17,7 +27,6 @@ app.use( '/insights', ghost({
 
 //Routes
 app.get('/', (req, res) => res.send('Great and nerdy things coming... stay tuned.'));
-app.get('/test', (req, res) => res.send('rendered with express'));
 
 //Port Configs
 app.set('port', (process.env.PORT || 5000));
