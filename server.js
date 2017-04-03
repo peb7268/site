1f8d9104c4fc0ec337c5267788950f83809d181c
peb7268 <peb7268@gmail.com>
2017-04-03 13:41:07 -0400

    wip

--- server.js
+++ server.js
@@ -2,80 +2,26 @@
 var express     = require('express');
 var path        = require('path');
 var app         = express();
+var ghost 		= require(__dirname + '/public/insights/ghost-middleware');
 
 //Middleware Configs
 app.use(express.static(__dirname + '/public'));
 
 //Ghost configs
-var debug 		= require('debug')('ghost:boot:index'), ghost, logging, errors, utils;
-var parentApp   = app;
-ghost       	= require(__dirname + '/public/insights/core');
-logging 		= require(__dirname + '/public/insights/core/server/logging');
-errors  		= require(__dirname + '/public/insights/core/server/errors');
-utils   		= require(__dirname + '/public/insights/core/server/utils');
-
+var config_path = path.join(__dirname, '/public/insights/config.js');
 
 console.log('===========================');
 console.log('Ghost diagnostics: ');
 console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
 console.log(`port: ${process.env.PORT}`);
+console.log(`config_path: ${config_path}`);
 console.log(ghost);
 console.log('===========================');
 
-console.log('Ghost custom boot');
-console.time('Ghost boot');
 
-debug('First requires...');
-debug('Required ghost');
-
-debug('Initialising Ghost');
-ghost().then(function (ghostServer) {
-    // Mount our Ghost instance on our desired subdirectory path if it exists.
-	var subdir = utils.url.getSubdir();
-	console.log(`subdir path: ${utils.url.getSubdir()}`);
-	
-    parentApp.use(subdir, ghostServer.rootApp);
-
-    debug('Starting Ghost');
-    // Let Ghost handle starting our server instance.
-    return ghostServer.start(parentApp).then(function afterStart() {
-        console.timeEnd('Ghost boot');
-        // if IPC messaging is enabled, ensure ghost sends message to parent
-        // process on successful start
-        if (process.send) {
-            process.send({started: true});
-        }
-    });
-}).catch(function (err) {
-    if (!errors.utils.isIgnitionError(err)) {
-        err = new errors.GhostError({err: err});
-    }
-
-    if (process.send) {
-        process.send({started: false, error: err.message});
-    }
-
-    logging.error(err);
-    process.exit(-1);
-});
-
-
-/*
-ghost({
-	config: path.join(__dirname, 'insights/config.js')
-}).then(function (ghostServer) {
-	console.log('');
-	console.log('');
-	console.log('ghost paths: ');
-	console.log(ghostServer.config.paths);
-	console.log('');
-	console.log('');
-
-    app.use('/insights', ghostServer.rootApp);
-    ghostServer.start(parentApp);
-});
-*/
-
+app.use( '/insights', ghost({
+	config: config_path
+}) );
 
 //If you want to use view engines
 //app.set('views', __dirname + '/views');
