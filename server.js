50876c471db4c999a06019fe18592da6287e1f50
peb7268 <peb7268@gmail.com>
2017-04-06 13:12:51 -0400

    wip

--- server.js
+++ server.js
@@ -36,12 +36,19 @@ app.use( '/insights', ghost({
 app.set('views', __dirname + '/public/views');
 app.set('view engine', 'pug');
 
-//app.get('/', (req, res) => res.render('pages/index'));
-
 //Routes
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => res.render('index', {active_page: 'home'}));
 app.get('/about', (req, res) => res.render('about', {message: 'Great and nerdy things coming... stay tuned.', title: 'this is how you pass data from express to your views.', active_page: 'about-page'}));
 app.get('/services', (req, res) => res.render('services', {active_page: 'services-page'}));
+app.get('/services/website-development', (req, res) => res.render('web-development', {active_page: 'services-page', sub_page: 'website-development' }));
+app.get('/services/application-development', (req, res) => res.render('appdev', {active_page: 'services-page', sub_page: 'appdev' }));
+app.get('/services/search-engine-optimization', (req, res) => res.render('seo', {active_page: 'services-page', sub_page: 'seo' }));
+app.get('/services/social-media-marketing', (req, res) => res.render('smm', {active_page: 'services-page', sub_page: 'smm' }));
+app.get('/services/digital-marketing-strategy', (req, res) => res.render('strategy', {active_page: 'services-page', sub_page: 'strategy' }));
+app.get('/services/copywriting', (req, res) => res.render('copywriting', {active_page: 'services-page', sub_page: 'copywriting' }));
+app.get('/services/branding', (req, res) => res.render('branding', {active_page: 'services-page', sub_page: 'branding' }));
+app.get('/services/website-rennovation', (req, res) => res.render('web-rennovation', {active_page: 'services-page', sub_page: 'web-rennovation' }));
+app.get('/services/mentoring', (req, res) => res.render('mentoring', {active_page: 'services-page', sub_page: 'mentoring' }));
 
 //Port Configs
 app.set('port', (process.env.PORT || 5000));
