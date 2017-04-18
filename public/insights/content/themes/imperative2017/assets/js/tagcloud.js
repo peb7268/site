0ce6566cb36ebdf6abb11ccdf0c7e74a87727d06
marius talpos <mariustalpos@mariuss-MacBook-Pro.local>
2017-04-18 19:46:26 -0400

    tag cloud wip

--- public/insights/content/themes/imperative2017/assets/js/tagcloud.js
+++ public/insights/content/themes/imperative2017/assets/js/tagcloud.js
@@ -1,35 +1,72 @@
 
-var host = "localhost";
-var user = "root";
-var password = "your_password_here";
-var database = "tagcloud";
-    
-//make connection
-var server = mysql_connect(host, user, password);
-var connection = mysql_select_db(database, server);
-    
-//query the database
-var query = mysql_query("SELECT * FROM tags");
+// console.log('tagcloud.js here');
+// window.onload = function() {
+//     if (window.jQuery) {  
+//         // jQuery is loaded  
+//         alert("Yeah!");
+//     } else {
+//         // jQuery is not loaded
+//         alert("Doesn't Work");
+//     }
+// }
+// set font size cap
+// divide number of tags used
+// smooth out with a factor
+// get font for tag
 
-var json = "({ tags:[";
-
-for ($x = 0; $x < mysql_num_rows($query); $x++) {
-    $row = mysql_fetch_assoc($query);
-            
-    //continue json object
-    $json += "{tag:'" . $row["tag"] + "',freq:'" + $row["frequency"] + "'}";
-            
-    //add comma if not last row, closing brackets if is
-    if ($x < mysql_num_rows($query) -1)
-        $json += ",";
-    else
-        $json += "]})";
-
-    var response = $.get(
-        "somepage.php",
-        {paramOne : 1, paramX : 'abc'},
-        function(data) {
-        alert('page content: ' + data);
+// get all tags from ghost
+// count frequency
+// map them
+// tags: {
+//  tagName: {frequency: x}
+//  url: http://url.com  
+// } 
+let createTagArray = function(posts){
+    let tagsArray = [];    
+    let post;
+    for(post in posts){
+        let tags = posts[post].tags;
+        let tag;
+        for(tag in tags){
+            let tagname = tags[tag].name
+            tagsArray.push(tagname);
         }
-    );
+    }
+    return tagsArray
 }
+
+let calculateFontSize = function(tagCount, tagsArray){
+    let totalTags = tagsArray.length;
+    let fontFactor = 50;
+    for(var key in tagCount) {
+        if(tagCount.hasOwnProperty(key)) {
+            element = tagCount[key]
+            tagCount[key] = (element/totalTags) * fontFactor;
+            console.log(tagCount[key])
+        }
+    }
+    let fonts = tagCount;
+    return fonts
+}
+
+let createTagCloud = function(){
+    $.get(ghost.url.api('posts', {include: 'tags'}))
+        .done(function (data){
+            let posts = data.posts;
+            let tags = createTagArray(posts);
+            let tagCount = {};
+            let i;
+            tags.forEach(function(i) { 
+                tagCount[i] = (tagCount[i] || 0)+1; 
+            });
+
+            let fonts = calculateFontSize(tagCount, tags);
+            console.log(fonts);
+
+        })
+        .fail(function (err){
+            console.log(err);
+        });
+}
+
+createTagCloud();
