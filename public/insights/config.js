a9ff52bb6997fb5a854d44ec3be3220814b9d807
peb7268 <peb7268@gmail.com>
2017-07-07 17:24:10 -0400

    heroku deploy

--- public/insights/config.js
+++ public/insights/config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'https://imperativedesign.net/insights',
+        url: 'http://imperativedesign.net/insights',
         fileStorage: false,
         mail: {
           transport: 'SMTP',
