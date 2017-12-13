3c09ecc0b72cd9f6c701ebbefd89cd4a4130fda7
Marius <exersist44@gmail.com>
2017-12-12 23:17:10 -0500

    heroku deploy

--- MigratorConfig.js
+++ MigratorConfig.js
@@ -4,11 +4,10 @@ module.exports = {
     database: {
         client: 'mysql',
         connection: {
-            host: '127.0.0.1',
-            user: 'user',
-            password: 'password',
-            charset: 'utf8',
-            database: 'ghost'
+            "host": "us-cdbr-iron-east-05.cleardb.net",
+            user: "b5d1e148a9ba84",
+            password: "48a7d98b",
+            database: "heroku_d6140dbbff83350"
         }
     },
     migrationPath: process.cwd() + '/current/core/server/data/migrations',
