f9855edb0c502335bab42f80563c548b5795b205
Paul Barrick <paul@imperativedesign.net>
2017-12-19 00:10:20 -0500

    heroku deploy

--- insights/index.js
+++
@@ -1,8 +0,0 @@
-var ghost = require('ghost');
-var path = require('path');
-
-console.log('==== this is where its all happening =====');
-
-ghost().then(function (ghostServer) {
-    ghostServer.start();
-});
