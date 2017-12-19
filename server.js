b7c16fc0642a1bc744c80242525629c4db363c8f
Paul Barrick <paul@imperativedesign.net>
2017-12-18 23:49:30 -0500

    heroku deploy

--- server.js
+++ server.js
@@ -175,4 +175,4 @@ app.post('/contact', (req, res)=>{
 
 //Port Configs
 app.set('port', (process.env.PORT || 5000));
-app.listen(app.get('port'), () => console.log('Node app is running on port', app.get('port'));
+app.listen(app.get('port'), () => console.log('Node app is running on port', app.get('port')));
