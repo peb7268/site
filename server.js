e6fac5bda487a0f7f244e0a6dc6e38c989d4cca1
Paul Barrick <paul@imperativedesign.net>
2017-12-19 11:20:59 -0500

    heroku deploy

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ let env_config = {
 console.log('Debug Info: ');
 console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
 console.log(`port: ${process.env.PORT}`);
-console.log(`===== prod db config is a ${typeof env_config.database} with a host of ${typeof env_config.database.connection.host} =======`);
+console.log(`===== prod db config is a ${typeof env_config.database} with a host of ${env_config.database.connection.host} =======`);
 console.log("");
 
 // //Init Ghost in a subdirectory
@@ -69,8 +69,8 @@ ghost(env_config).then((ghostServer) => {
 	console.log("==== In ghost bootup =====");
 	// app.use(utils.url.getSubdir(), ghostServer.rootApp);
 	
-	ghostServer.config.set('database:connection', env_config.database.connection);
-	ghostServer.config.set('database:connection:user', env_config.database.connection.user);
+	ghostServer.config.set('ghost:config:database:connection', env_config.database.connection);
+	ghostServer.config.set('config:database:connection:user', env_config.database.connection.user);
 	ghostServer.config.set('database.connection.password', env_config.database.connection.password);
 
 	
